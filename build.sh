b8347b5249a6175689bd459c0ac08803e3072fba
Ethan Cotterell <ethancotterell@gmail.com>
2021-03-02 17:38:21 +0000

    added push and deploy commands to build script

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ faas-cli build \
     -f instaloader.yml \
     --build-arg ADDITIONAL_PACKAGE='gcc musl-dev python3-dev libffi-dev openssl-dev cargo'
 
+docker push ghcr.io/flapjack321/fn-instaloader
+
+faas-cli deploy -f instaloader.yml
